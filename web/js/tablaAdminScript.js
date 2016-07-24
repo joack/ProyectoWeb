b45e6b0598842976b89fc968b03d8495aacd4a6d
Joack <Joack@Joack-Notebok>
2016-07-24 00:26:44 -0300

    Agregado de archivo Script : "tablaAdminScript.js"

---
+++ web/js/tablaAdminScript.js
@@ -0,0 +1,64 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function () {
+    $("#addUser_form").on("submit", function(e) 
+    {
+        var postData = $(this).serializeArray();
+        var formURL = $(this).attr("action");
+        $.ajax({
+            url: formURL,
+            type: "POST",
+            data: postData,
+            success: function(data, textStatus, jqXHR) {
+                $('#addUser_dialog .modal-header .modal-title').html("Result");
+                $('#addUser_dialog .modal-body').html(data);
+                $("#submitAddUserForm").remove();
+            },
+            error: function(jqXHR, status, error) {
+                console.log(status + ": " + error);
+            }
+        });
+        e.preventDefault();
+    });
+    $("#submitAddUserForm").on('click', 
+        function() 
+        {
+            $("#addUser_form").submit();
+        }
+
+    );
+});
+
+
+$(document).ready(function () {
+    $("#productAdd_form").on("submit", function(e) {
+        var postData = $(this).serializeArray();
+        var formURL = $(this).attr("action");
+        $.ajax({
+            url: formURL,
+            type: "POST",
+            data: postData,
+            success: function(data, textStatus, jqXHR) {
+                $('#contact_dialog .modal-header .modal-title').html("Result");
+                $('#contact_dialog .modal-body').html(data);
+                $("#submitForm").remove();
+            },
+            error: function(jqXHR, status, error) {
+                console.log(status + ": " + error);
+            }
+        });
+        e.preventDefault();
+    });
+
+    $("#submitProductForm").on('click', 
+        function() 
+        {
+            $("#contact_form").submit();
+        }
+
+    );
+});
